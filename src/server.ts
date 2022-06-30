a1891a730da9a82ccbc7f24b2787838eed3c4072
duzate <duzate@gmail.com>
2022-06-30 16:59:34 -0300

    server file change

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,13 @@ import { AppDataSource } from './database'
 
 AppDataSource.initialize()
   .then(() => {
-    console.log("Data Source has been initialized!")
+    console.log("Data Source has been initialized!");
+    app.listen(3333, () => {
+      console.log("Server is running http://localhost:3333");
+    })
   })
   .catch((err) => {
     console.error("Error during Data Source initialization", err)
   })
 
-app.listen(3333, () => {
-  console.log("Server is running http://localhost:3333");
-})
+
